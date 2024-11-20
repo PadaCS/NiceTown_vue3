b44b1b567f03809eec4da40fe35c54a4ca64f294
Kannshya <jp2021212716@qmul.ac.uk>
2024-11-20 22:29:25 +0800

    更新了搜索功能, 仅看我的功能, 在我的宣传里搜索的功能

--- src/api/promote.ts
+++ src/api/promote.ts
@@ -1,7 +1,7 @@
 // 导入请求工具
 import request from '@/util/request'
 
-// 提供调用注册接口的函数
+// 提供调用list接口的函数
 export const promoteListService = (
     params:{ pageNum: number; pageSize: number; promoteType?: string }
 ) => {
@@ -9,12 +9,17 @@ export const promoteListService = (
   return request.get('/promote/list', {params});
 };
 
-
-import type { Promote } from '@/Types/types';
-
 // 提供调用匹配乡镇接口的函数
 export const townListService = (
   townIDs: number[]
 ) => {
   return request.post('/promote/town', townIDs);
 }
+
+// 提供调用listmy接口的函数
+export const viewMyService = (
+  params:{ pageNum: number; pageSize: number; promoteType?: string }
+) => {
+  // 返回 GET 请求
+return request.get('/promote/listmy', {params});
+};
--- src/views/Promote/Promote.vue
+++ src/views/Promote/Promote.vue
@@ -1,6 +1,6 @@
 <script setup lang="ts">
     import { ref } from 'vue'
-    const isCustomer = ref(false)
+    const isPromoter = ref(false)
 
     import {
         Edit,
@@ -8,7 +8,11 @@
         // HelpFilled
     } from '@element-plus/icons-vue'
 
-    import { promoteListService, townListService } from '@/api/promote'
+    // ———————————————————————————————————————————————————————————
+    // ——————————————————————数据回显相关功能——————————————————————
+    // ———————————————————————————————————————————————————————————
+
+    import { promoteListService, townListService, viewMyService } from '@/api/promote'
     import type { Promote, Towns } from '@/Types/types'; // 定义 Promote 类型
     
     // 定义 promotes 和 towns 类型
@@ -17,12 +21,17 @@
     
     const promoteList = async()=>{
         console.log('promoteList被调用了')
+        isPromoter.value = false;
         // 之后要写成带页码参数的形式
         let result = await promoteListService({pageNum:1, pageSize:10});
         // let result = await promoteListService({pageNum:1, pageSize:5, promoteType:'农家院'});
         promotes.value = result.data.items || [];
+        total.value = result.data.total || 0;
+        
         // 调用 townList 获取 towns 数据
-        await townList();
+        if(promotes.value != null){
+            await townList();
+        }
 
         // 为 promotes 添加 townFullName
         promotes.value.forEach((promote) => {
@@ -44,9 +53,127 @@
         // 调用 townListService 获取 towns 数据
         let result = await townListService(townIDs);
         towns.value = result.data || [];
+        // let result
+        // if(townIDs != null){
+        //     result = await townListService(townIDs);
+        //     towns.value = result.data || [];
+        // }else{
+        //     result = null
+        // }
         console.log('towns:', towns.value);
 
     };
+
+
+
+    // 仅看我发布的:
+    const viewMy = async()=>{
+        console.log('viewMy被调用了')
+        isPromoter.value = true;
+        // 之后要写成带页码参数的形式
+        let result = await viewMyService({pageNum:1, pageSize:10});
+        // let result = await promoteListService({pageNum:1, pageSize:5, promoteType:'农家院'});
+        promotes.value = result.data.items || [];
+        total.value = result.data.total || 0;
+        
+        // 调用 townList 获取 towns 数据
+        if(promotes.value != null){
+            await townList();
+        }
+        // 为 promotes 添加 townFullName
+        promotes.value.forEach((promote) => {
+            const town = towns.value.find((t) => t.townID === promote.townID);
+            promote.townFullName = town ? `${town.province}${town.city}${town.name}` : '未知乡镇';
+        });
+        console.log('result:\n' + promotes.value)
+    }
+
+
+    // ———————————————————————————————————————————————————————————
+    // ——————————————————————搜索栏相关功能——————————————————————
+    // ———————————————————————————————————————————————————————————
+    //用户搜索时选中的宣传类型id
+    const categoryId=ref('')
+
+
+    //宣传类型数据模型
+    const categorys = ref([
+        {
+            id: 1,
+            categoryName: "农家院"
+        },
+        {
+            id: 2,
+            categoryName: "自然风光秀丽"
+        },
+        {
+            id: 3,
+            categoryName: "古建筑"
+        },
+        {
+            id: 4,
+            categoryName: "土特产"
+        },
+        {
+            id: 5,
+            categoryName: "特色小吃"
+        },
+        {
+            id: 6,
+            categoryName: "民俗活动"
+        }
+    ]);
+
+    const getCategoryNameById = (id:any) => {
+        const category = categorys.value.find((item) => item.id === id);
+        return category?.categoryName;
+    };
+
+    const search = async()=>{
+        console.log('search被调用了\n')
+        // 判断是否为"我的"页面
+        let result
+        if(isPromoter.value == false){
+            result = await promoteListService({pageNum:1, pageSize:10, promoteType:getCategoryNameById(categoryId.value)});
+        }else{
+            result = await viewMyService({pageNum:1, pageSize:10, promoteType:getCategoryNameById(categoryId.value)});
+        }
+
+        promotes.value = result.data.items || [];
+        total.value = result.data.total || 0;
+        
+        // 调用 townList 获取 towns 数据
+        if(promotes.value != null){
+            await townList();
+        }
+
+        // 为 promotes 添加 townFullName
+        promotes.value.forEach((promote) => {
+            const town = towns.value.find((t) => t.townID === promote.townID);
+            promote.townFullName = town ? `${town.province}${town.city}${town.name}` : '未知乡镇';
+        });
+        console.log('result:\n' + promotes.value)
+    }
+
+    // ———————————————————————————————————————————————————————————
+    // ——————————————————————分页展示相关功能——————————————————————
+    // ———————————————————————————————————————————————————————————
+
+    //分页条数据模型
+    const pageNum = ref(1)//当前页
+    const total = ref(20)//总条数
+    const pageSize = ref(6)//每页条数
+
+    //当每页条数发生了变化，调用此函数
+    const onSizeChange = (size:number) => {
+        pageSize.value = size
+    }
+    //当前页码发生变化，调用此函数
+    const onCurrentChange = (num:number) => {
+        pageNum.value = num
+    }
+
+
 </script>
 <template>
     <el-card class="page-container">
@@ -54,18 +181,40 @@
             <div class="header">
                 <span>乡镇宣传</span>
                 <div class="extra">
-                    <el-button type="primary">仅看我发布的</el-button>
+                    <el-button type="primary" @click="promoteList"  v-if="isPromoter">查看全部</el-button>
+                    <el-button type="primary" @click="viewMy"  v-else>仅看我发布的</el-button>
+
+                    <el-button type="primary">发布宣传</el-button>
                 </div>
             </div>
         </template>
+        <!-- ——————————————————————搜索栏—————————————————————— -->
+        <el-form inline>
+            <el-form-item label="宣传类型：" class="type">
+                <el-select placeholder="请选择" v-model="categoryId">
+                    <el-option 
+                        v-for="c in categorys" 
+                        :key="c.id" 
+                        :label="c.categoryName"
+                        :value="c.id">
+                    </el-option>
+                </el-select>
+            </el-form-item>
+            <el-form-item>
+                <el-button type="primary" @click="search">搜索</el-button>
+                <el-button>重置</el-button>
+            </el-form-item>
+        </el-form>
+
+        <!-- ——————————————————————文章列表—————————————————————— -->
         <el-table :data="promotes" style="width: 100%">
             <el-table-column label="序号" width="100" type="index"theme> </el-table-column>
             <el-table-column label="主题" prop="theme"></el-table-column>
             <el-table-column label="乡镇" prop="townFullName"></el-table-column>
             <el-table-column label="类型" prop="promoteType"></el-table-column>
             <!-- 这一行只在查看我自己的宣传的时候显示 -->
-            <el-table-column label="操作" width="100" v-if="isCustomer">
-                <!-- <template #default="{ row }" v-if="isCustomer"> -->
+            <el-table-column label="操作" width="100" v-if="isPromoter">
+                <!-- <template #default="{ row }" v-if="isPromoter"> -->
                 <template #default="{ row }">
                     <el-button :icon="Edit" circle plain type="primary"></el-button>
                     <el-button :icon="Delete" circle plain type="danger"></el-button>
@@ -78,6 +227,14 @@
                 <el-empty description="没有数据" />
             </template>
         </el-table>
+
+        <!-- ——————————————————————分页条—————————————————————— -->
+
+        <el-pagination v-model:current-page="pageNum" v-model:page-size="pageSize" :page-sizes="[3,6,9]"
+            layout="jumper, total, sizes, prev, pager, next" background :total="total" @size-change="onSizeChange"
+            @current-change="onCurrentChange" style="margin-top: 20px; justify-content: flex-end" />
+
+
     </el-card>
 </template>
 
@@ -85,6 +242,10 @@
 .page-container {
     min-height: 100%;
     box-sizing: border-box;
+
+    .type{
+        min-width: 30dvh;
+    }
 
     .header {
         display: flex;
